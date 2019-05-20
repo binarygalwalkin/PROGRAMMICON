93422fed1b4d447b9ef250989621a39503f6db40
binarygalwalkin <44074053+binarygalwalkin@users.noreply.github.com>
2019-05-20 16:27:49 -0700

    Add files via upload

---
+++ C#/Code_Examples/ch14code/ch14code/Fig14_30/PictureBoxTest/PictureBoxTest/PictureBoxTestForm.cs
@@ -0,0 +1,46 @@
+﻿// Fig. 14.30: PictureBoxTestForm.cs
+// Using a PictureBox to display images.
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PictureBoxTest
+{
+   // Form to display different images when PictureBox is clicked
+   public partial class PictureBoxTestForm : Form
+   {
+      private int imageNum = -1; // determines which image is displayed
+
+      // default constructor
+      public PictureBoxTestForm()
+      {
+         InitializeComponent();
+      } // end constructor
+
+      // change image whenever Next Button is clicked
+      private void nextButton_Click( object sender, EventArgs e )
+      {
+         imageNum = ( imageNum + 1 ) % 3; // imageNum cycles from 0 to 2
+
+         // create Image object from file, display in PicutreBox
+         imagePictureBox.Image = ( Image )
+            ( Properties.Resources.ResourceManager.GetObject(
+            string.Format( "image{0}", imageNum ) ) );
+      } // end method nextButton_Click
+   } // end class PictureBoxTestForm
+} // end namespace PictureBoxTest
+
+/**************************************************************************
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
---
+++ C#/Code_Examples/ch14code/ch14code/Fig14_38/Painter/Painter/PainterForm.cs
@@ -0,0 +1,65 @@
+﻿// Fig 14.38: PainterForm.cs
+// Using the mouse to draw on a Form.
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Painter
+{
+   // creates a Form that is a drawing surface
+   public partial class PainterForm : Form
+   {
+      bool shouldPaint = false; // determines whether to paint
+
+      // default constructor
+      public PainterForm()
+      {
+         InitializeComponent();
+      } // end constructor
+
+      // should paint when mouse button is pressed down
+      private void PainterForm_MouseDown( 
+         object sender, MouseEventArgs e )
+      {
+         // indicate that user is dragging the mouse
+         shouldPaint = true;
+      } // end method Painter_MouseDown
+
+      // stop painting when mouse button is released
+      private void PainterForm_MouseUp( object sender, MouseEventArgs e )
+      {
+         // indicate that user released the mouse button
+         shouldPaint = false;
+      } // end method Painter_MouseUp
+
+      // draw circle whenever mouse moves with its button held down        
+      private void PainterForm_MouseMove( 
+         object sender, MouseEventArgs e )
+      {
+         if ( shouldPaint ) // check if mouse button is being pressed
+         {
+            // draw a circle where the mouse pointer is present
+            using ( Graphics graphics = CreateGraphics() )
+            {
+               graphics.FillEllipse(
+                  new SolidBrush( Color.BlueViolet ), e.X, e.Y, 4, 4 );
+            } // end using; calls graphics.Dispose()
+         } // end if
+      } // end method Painter_MouseMove
+   } // end class PainterForm
+} // end namespace Painter
+
+/**************************************************************************
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
