0352bc1af7f1a3b300f05ce7e9124e1dd9be2bb5
binarygalwalkin <44074053+binarygalwalkin@users.noreply.github.com>
2019-05-20 17:21:54 -0700

    Add files via upload

---
+++ C#/Code_Examples/wpfmvvm/wpfmvvm/Messages/ViewModelMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace wpfmvvm.Messages
+{
+    class ViewModelMessage: MessageBase
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ C#/Code_Examples/wpfmvvm/wpfmvvm/ViewModel/ReceiverViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight; //For mvvmlight 
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;//for class Messenger
+using wpfmvvm.Messages;
+
+namespace wpfmvvm.ViewModel
+{
+    public class ReceiverViewModel : ViewModelBase
+    {
+        private string _contentText;
+
+        public string ContentText
+        {
+            get { return _contentText; }
+            set
+            {
+                _contentText = value;
+                RaisePropertyChanged("ContentText");
+            }
+        }
+
+        public ReceiverViewModel()
+        {
+            Messenger.Default.Register<ViewModelMessage>(this, OnReceiveMessageAction);
+        }
+
+        private void OnReceiveMessageAction(ViewModelMessage obj)
+        {
+            ContentText = obj.Text;
+        }
+    }
+}
