7817d269899e61e5d1f6fef50b7a51502e4d6532
binarygalwalkin <44074053+binarygalwalkin@users.noreply.github.com>
2019-05-20 17:17:50 -0700

    Add files via upload

---
+++ C#/Code_Examples/mvvmwpfdemos/ItemsControlDataTemplateTest/StudentListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace ItemsControlDataTemplateTest
+{
+    public class StudentListViewModel
+    {
+        public ObservableCollection<StudentData> TheStudents { get; private set; }
+
+        public StudentListViewModel()
+        {
+            TheStudents = new ObservableCollection<StudentData>();
+        }
+    }
+}
