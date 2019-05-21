194dd101e084ab9b4417687cfc0906d286b3a57d
binarygalwalkin <44074053+binarygalwalkin@users.noreply.github.com>
2019-05-20 17:17:07 -0700

    Add files via upload

---
+++ C#/Code_Examples/mvvmwpfdemos/CommandSample/ViewModel/CommandSampleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace CommandSample
+{
+    public class CommandSampleViewModel
+    {
+        public DelegateCommand TheCommand { get; private set; }
+
+        public CommandSampleViewModel()
+        {
+            TheCommand = new DelegateCommand(OnExecute, CanExecute);
+        }
+
+        bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        void OnExecute()
+        {
+            MessageBox.Show("Command Executed");
+        }
+    }
+}
